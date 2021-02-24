ffa14feb317cb23ca3eb977b6a35f2df838d8f1b
moonbyte <moonbyt3@gmail.com>
2021-02-24 18:45:06 +0100

    Fonts

--- components/Sidebar/Sidebar.css.js
+++ components/Sidebar/Sidebar.css.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
 export const SidebarWrapper = styled.div`
-
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100%;
+    max-width: 350px;
+    width: 100%;
+    background-color: blue;
+    padding: 25px;
+    font-family: 'DIN Next LT Pro';
 `;
